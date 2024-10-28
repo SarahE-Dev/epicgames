07ff21b8013e877e525680e8d217ca7e7311be19
Sarah E <seatherly.prsvr@gmail.com>
2024-10-28 16:21:57 -0500

    done for tody, got logout finally working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,7 @@ class HomeController < ApplicationController
   def index
     @projects = Project.all
   end
+  
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   enum role: { user: 0, admin: 1 }
+
+  
   
   def admin?
     role == "admin"
